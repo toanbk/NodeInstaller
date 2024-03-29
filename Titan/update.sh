0e3d698c80ab8c79ab161d7cf84200e3dc42eee4
Toan Pham <toan@betanet.co.il>
2024-03-29 16:35:43 +0700

    update titan

--- Titan/update.sh
+++ Titan/update.sh
@@ -8,7 +8,7 @@ echo -e "\e[1m\e[32m2. Install Node --> \e[0m" && sleep 1
 
 sudo systemctl stop "$BINARY_NAME"
 
-sudo wget https://github.com/Titannet-dao/titan-node/releases/download/${VERSION}/titan_v${VERSION}_linux_amd64.tar.gz
+sudo wget https://github.com/Titannet-dao/titan-node/releases/download/v${VERSION}/titan_v${VERSION}_linux_amd64.tar.gz
 sudo tar -xvzf titan_v${VERSION}_linux_amd64.tar.gz && rm -rf titan_v${VERSION}_linux_amd64.tar.gz
 sudo chmod +x titan_v${VERSION}_linux_amd64/*
 sudo cp -rf titan_v${VERSION}_linux_amd64/* /usr/local/bin/
