ceac778009a7c331713b40b4c4c5b3f73501b1f8
Toan Pham <toan@betanet.co.il>
2024-03-29 16:48:11 +0700

    update titan support arm

--- Titan/update.sh
+++ Titan/update.sh
@@ -4,15 +4,26 @@
 VERSION="0.1.15"
 BINARY_NAME="titand"
 
-echo -e "\e[1m\e[32m2. Install Node --> \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Update Node --> \e[0m" && sleep 1
+
+# Detect platform and save to variable
+if [[ $(uname -m) == "aarch64" ]]; then
+    platform="arm64"
+elif [[ $(uname -m) == "x86_64" ]]; then
+    platform="amd64"
+else
+    platform="unknown"
+fi
+
+FILE_NAME="titan_v${VERSION}_linux_${platform}"
 
 sudo systemctl stop "$BINARY_NAME"
 
-sudo wget https://github.com/Titannet-dao/titan-node/releases/download/v${VERSION}/titan_v${VERSION}_linux_amd64.tar.gz
-sudo tar -xvzf titan_v${VERSION}_linux_amd64.tar.gz && rm -rf titan_v${VERSION}_linux_amd64.tar.gz
-sudo chmod +x titan_v${VERSION}_linux_amd64/*
-sudo cp -rf titan_v${VERSION}_linux_amd64/* /usr/local/bin/
-sudo rm -rf titan_v${VERSION}_linux_amd64
+sudo wget https://github.com/Titannet-dao/titan-node/releases/download/v${VERSION}/${FILE_NAME}.tar.gz
+sudo tar -xvzf ${FILE_NAME}.tar.gz && rm -rf ${FILE_NAME}.tar.gz
+sudo chmod +x ${FILE_NAME}/*
+sudo cp -rf ${FILE_NAME}/* /usr/local/bin/
+sudo rm -rf ${FILE_NAME}
 
 sudo systemctl restart "$BINARY_NAME"
 
